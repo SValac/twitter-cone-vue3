8dc9f4e4c1c6241dd1fa9a3dfd30f1cbe5bd1e6e
SValac <shiro.valac@gmail.com>
2023-12-23 00:03:36 -0600

    adding tweetFeedList to main section

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <script setup>
 import { ref } from 'vue';
+const { twitterBorderColor } = useTailwindConfig();
+const { getHomeTweets } = useTweets();
 const loading = ref(false);
 const { useAuthUser } = useAuth();
 
+const homeTweets = ref([{ 2: 2 }, { 2: 1 }]);
+
 const user = useAuthUser();
+
+onBeforeMount(async () => {
+	loading.value = true;
+	try {
+		const { tweets } = await getHomeTweets();
+		homeTweets.value = tweets;
+	} catch (error) {
+		console.log(error);
+	} finally {
+		loading.value = false;
+	}
+});
 </script>
 
 <template>
@@ -12,8 +28,16 @@ const user = useAuthUser();
 			title="Home"
 			:loading="loading"
 		>
-			<!-- {{ user }} -->
-			<TweetForm :user="user" />
+			<Head>
+				<Title>Home / Twitter</Title>
+			</Head>
+			<section
+				class="border-b"
+				:class="twitterBorderColor"
+			>
+				<TweetForm :user="user" />
+			</section>
+			<TweetListFeed :tweets="homeTweets" />
 		</MainSection>
 	</div>
 </template>
