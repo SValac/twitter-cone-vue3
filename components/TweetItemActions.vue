5f81792aa3a811bd8da944e0f9d221e7e63136d9
SValac <shiro.valac@gmail.com>
2023-12-26 08:39:31 -0600

    action slot grabing the props from child component and using in parent

--- components/TweetItemActions.vue
+++ components/TweetItemActions.vue
@@ -1,5 +1,38 @@
+<script setup>
+import {
+	ChatBubbleOvalLeftEllipsisIcon as CommentIcon,
+	ArrowPathIcon as RefreshIcon,
+	HeartIcon as LikeIcon,
+	ArrowUpTrayIcon as UploadIcon
+} from '@heroicons/vue/24/outline';
+</script>
 <template>
-	<div>
-		<TweetItemActionsIcon color="green" />
+	<div class="flex items-center justify-around w-full">
+		<TweetItemActionsIcon color="blue">
+			<!-- iconSlot is a name variable which has props from icon slot -->
+			<template v-slot:icon="iconSlot">
+				<CommentIcon :class="iconSlot.classes" />
+			</template>
+			<template v-slot:default> 9 </template>
+		</TweetItemActionsIcon>
+		<!-- using destructurin to extract classses -->
+		<TweetItemActionsIcon color="green">
+			<template v-slot:icon="{ classes }">
+				<RefreshIcon :class="classes" />
+			</template>
+			<template v-slot:default> 9 </template>
+		</TweetItemActionsIcon>
+		<TweetItemActionsIcon color="red">
+			<template v-slot:icon="{ classes }">
+				<LikeIcon :class="classes" />
+			</template>
+			<template v-slot:default> 9 </template>
+		</TweetItemActionsIcon>
+		<TweetItemActionsIcon color="blue">
+			<template v-slot:icon="{ classes }">
+				<UploadIcon :class="classes" />
+			</template>
+			<template v-slot:default> 9 </template>
+		</TweetItemActionsIcon>
 	</div>
 </template>
--- components/TweetItemActionsIcon.vue
+++ components/TweetItemActionsIcon.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/vue/24/solid';
 const { defaultTransition } = useTailwindConfig();
 const props = defineProps({
 	color: {
@@ -13,8 +12,13 @@ const props = defineProps({
 		<div
 			:class="`p-2 group-hover:bg-${props.color}-100 group-hover:text-${props.color}-400 rounded-full ${defaultTransition} dark:group-hover:bg-opacity-20`"
 		>
-			<ChatBubbleOvalLeftEllipsisIcon class="w-5 h-5" />
+			<slot
+				name="icon"
+				classes="w-5 h-5"
+			/>
 		</div>
-		<span :class="`ml-1 group-hover:text-${props.color}-400`">5</span>
+		<span :class="`ml-1 group-hover:text-${props.color}-400`">
+			<slot></slot>
+		</span>
 	</div>
 </template>
