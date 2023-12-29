1c1ad5edc2b6fad4c2794e0fb8a0d835af0be3d0
SValac <shiro.valac@gmail.com>
2023-12-29 12:41:03 -0600

    adding  getTweetByIt page to  auth middleware, and including author, replies and reply to endpoint

--- server/db/tweets.js
+++ server/db/tweets.js
@@ -14,10 +14,14 @@ export const getTweets = (params = {}) => {
 	});
 };
 
-export const getTweetById = (tweetId) => {
+export const getTweetById = (tweetId, params = {}) => {
 	return prisma.tweet.findUnique({
+		...params,
 		where: {
+			// spread paramse wirthout overridend id
+			...params.where,
 			id: tweetId
 		}
+		// ...params spreading params after where will override all whithin where key so it shuld be before where
 	});
 };
