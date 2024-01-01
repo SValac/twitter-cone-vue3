759605e42c1b4c4cb14b920f923fdcbbdf90809b
SValac <shiro.valac@gmail.com>
2024-01-01 17:43:58 -0600

    watch URL change to navigate to new tweet and getTweet

--- pages/status/[id].vue
+++ pages/status/[id].vue
@@ -5,6 +5,12 @@ const user = useAuthUser();
 const loading = ref(false);
 const tweet = ref(null);
 
+// watch for URL change to navigate get new tweet
+watch(
+	() => useRoute().fullPath,
+	() => getTweet()
+);
+
 // this is not a compued because we want the lastest tweet every time its called
 function getTweetIdFromRoute() {
 	return useRoute().params.id;
